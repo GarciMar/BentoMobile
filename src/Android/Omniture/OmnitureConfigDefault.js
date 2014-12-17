7a63e32937bb265e38e75c9584c9adc5c3a51509
GarciMar <Maribel.Garcia@viacommix.com>
2014-12-17 15:30:00 -0500

    Update OmnitureConfigDefault.js

--- src/Android/Omniture/OmnitureConfigDefault.js
+++ src/Android/Omniture/OmnitureConfigDefault.js
@@ -7,7 +7,7 @@
         "omnitureCurrencyCode" : "USD",
         "omnitureSSL" : "false",
         "omnitureLifecycleSessionTimeout" : "3000",
-        "omnitureTrackingServer" : "sc.mtv.com",
+        "omnitureTrackingServer" : "",
         "omnitureVisitorID" : "testcc8fb4594e6e98c4a9034a2f8f7d",
         "omnitureDebugLogging" : "true",
         "omnitureOfflineTrackingEnabled" : "true",
